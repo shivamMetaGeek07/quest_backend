628b30ca5ab66d717f73069014d53bc966274438
shivam <shivam@metageeks.tech>
2024-06-29 13:26:31 +0530

    kols routes,controller added

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,37 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-
+import mongoose from 'mongoose';
+import kolsRouter from './routes/kols/kols';
+import cors from 'cors';
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;;
 
+if (!process.env.MONGO_URL) {
+  throw new Error('MONGODB_URI is not defined in the environment variables');
+}
+const mongoUri: string = process.env.MONGO_URL!;
+
+app.use(express.json());
+app.use(cors());
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript server');
 });
 app.get('/greet', (req: Request, res: Response) => {
   res.send('Express + TypeScript server says Hello');
 });
+
+app.use('/kols', kolsRouter);
+
+
+mongoose.connect(mongoUri, {
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('Failed to connect to MongoDB', err);
+});
+
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server iS running at http://localhost:${port}`);
