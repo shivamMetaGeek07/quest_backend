651e16c4b6a3a8e538c38f037f426a08e7737769
shivam <shivam@metageeks.tech>
2024-07-05 16:37:41 +0530

    description removed

--- src/models/admin/communityData.ts
+++ src/models/admin/communityData.ts
@@ -3,13 +3,11 @@ import { Schema, model, Document } from 'mongoose';
 interface ICommunity extends Document {
   categories: string[];
   ecosystems: string[];
-  description?: string;
 }
 
 const communitySchema = new Schema<ICommunity>({
   categories: { type: [String], required: true },
   ecosystems: { type: [String], required: true },
-  description: { type: String },
 });
 
 const CommunityData = model<ICommunity>('CommunityData', communitySchema);
