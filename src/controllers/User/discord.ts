e91ee1803eb38db7b5ec484755cd00e0ffe8f9e8
DiwanshGupta <dewanshgupta120@gmail.com>
2024-07-16 21:21:11 +0530

    hardcode discord

--- src/controllers/User/discord.ts
+++ src/controllers/User/discord.ts
@@ -41,7 +41,7 @@ client.on('shardError', (error) => {
   console.error('WebSocket connection error:', error);
 });
 
-// Log in the Discord client with the bot token 
+// Log in the Discord client with the bot token ere
 client.login(process.env.DISCORD_TOKEN as string);
 
 
