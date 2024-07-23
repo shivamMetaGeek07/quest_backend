94134c0e81882cb322ca3b8ece84379a0bc96a1f
sajidmeta <sajid@metageeks.tech>
2024-07-23 10:05:06 +0530

    badges api

--- src/routes/admin/admin.ts
+++ src/routes/admin/admin.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { login, signup } from '../../controllers/admin/adminControllers';
 import { getCommunityData,addCommunityData,updateById,deleteById } from '../../controllers/admin/communityData';
 import { addFeed, deleteFeed, updateFeed } from '../../controllers/feeds/feed';
+import {getBadges, addBadge,updateBadgeById,deleteBadgeById} from '../../controllers/admin/badgeController';
 const router = express.Router();
 
 // Register admin
@@ -16,6 +17,12 @@ router.delete('/metadata/:type/:id', deleteById);
 router.post('/add-feed',addFeed);
 router.patch( '/update-feed/:id', updateFeed );
 router.delete( '/delete-feed/:id  ', deleteFeed );
+//badge
+router.get('/badges', getBadges);
+router.post('/badges', addBadge);
+router.put('/badges/:id', updateBadgeById);
+router.delete('/badges/:id', deleteBadgeById);
+
 
 
 export default router;
